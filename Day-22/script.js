b4f1c243c4bd4a1b86aaa8334de7af08000966e1
Tya77 <thanhdatttlim@gmail.com>
2024-09-24 22:04:04 +0700

    chữa bài tập buổi 22

--- Day-22/script.js
+++ Day-22/script.js
@@ -1,87 +1,154 @@
-const app_form = document.querySelector(".app_form");
-const app_input = document.querySelector(".app_input");
-const btn_add = document.querySelector(".btn_add");
-const edit = document.querySelector(".edit");
-const delet = document.querySelector(".delete");
-const task_form = document.querySelector(".task_form");
-const task_wrapper = document.querySelector(".task_wrapper");
-const task_input = document.querySelector(".task_input");
-const task_list = document.querySelector(".task_list");
+var todoForm = document.querySelector(".TodoForm");
+var todoList = document.querySelector(".todo-list");
 
-function addTask() {
-  const task_wrapper = document.createElement("div");
-  task_wrapper.classList.add("task_wrapper");
+var todos = [];
 
-  const task_input = document.createElement("input");
-  task_input.type = "text";
-  task_input.value = app_input.value;
-  task_input.setAttribute("readonly", "readonly");
-  task_input.classList.add("task_input");
-  task_input.style.width = "85%";
+var formUpdateIds = [];
 
-  const edit = document.createElement("i");
-  edit.classList.add("fa-solid");
-  edit.classList.add("fa-pen-to-square");
+var renderTodos = function () {
+  var html = todos
+    .map(function (todo, index) {
+      var value = formUpdateIds.find(function (element) {
+        return +element.index === +index;
+      });
 
-  const delet = document.createElement("i");
-  delet.classList.add("fa-solid");
-  delet.classList.add("fa-trash");
+      if (value) {
+        return `<form class="TodoForm"><input type="text" class="todo-input" placeholder="Update task" value="${value.name}"><button type="submit" class="todo-btn">Add Task</button></form>`;
+      }
 
-  const label = document.createElement("label");
-  label.innerText = "Add Task";
-  label.style.display = "none";
-  label.style.cursor = "pointer";
-  label.classList.add("btn_task");
+      return `<div class="Todo" data-index="${index}">
+    <p data-type="completed" class="${
+      todo.completed ? "completed" : ""
+    }">${todo.name}</p>
+    <div>
+    <svg data-type="update" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen-to-square" class="svg-inline--fa fa-pen-to-square " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path fill="currentColor" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"></path>
+      </svg>
+      <svg data-type="remove" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path fill="currentColor" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+      </svg></div>
+  </div>`;
+    })
+    .join("");
 
-  edit.addEventListener("click", function () {
-    task_input.removeAttribute("readonly");
-    task_input.focus();
-    task_input.style.background = "none";
-    task_input.style.border = "1px solid #8758ff";
-    task_input.style.padding = "8px 16px";
-    task_input.style.width = "79%";
-    task_wrapper.style.background = "none";
-    task_wrapper.style.padding = "0px";
-    // task_wrapper.style.justifyContent = "initial";
+  todoList.innerHTML = html;
+};
 
-    edit.style.display = "none";
-    delet.style.display = "none";
-    label.style.display = "block";
-    label.style.background = "#8758ff";
-    label.style.paddingTop = "7.7px";
-    label.style.paddingLeft = "7px";
+var getTodoHtml = function (todo, index) {
+  return `<div class="Todo" data-index="${index}">
+    <p data-type="completed" class="${todo.completed ? "completed" : ""}">${
+    todo.name
+  }</p>
+    <div>
+    <svg data-type="update" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen-to-square" class="svg-inline--fa fa-pen-to-square " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path fill="currentColor" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"></path>
+      </svg>
+      <svg data-type="remove" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path fill="currentColor" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+      </svg></div>
+  </div>`;
+};
+
+var handleRemove = function (index) {
+  if (confirm("Bạn có chắc chắn?")) {
+    todos.splice(index, 1);
+    renderTodos();
+  }
+};
+
+var handleCompleted = function (index) {
+  todos[index].completed = !todos[index].completed;
+  renderTodos();
+};
+
+var showFormUpdate = function (todoEl) {
+  var index = todoEl.dataset.index;
+  var todo = todos[index];
+
+  var formUpdate = `<form class="TodoForm" data-index="${index}"><input type="text" class="todo-input" placeholder="Update task" value="${todo.name}"><button type="submit" class="todo-btn">Add Task</button></form>`;
+
+  todoEl.outerHTML = formUpdate;
+
+  var formList = todoList.querySelectorAll(".TodoForm");
+  formList.forEach(function (todoForm) {
+    if (todoForm.dataset.index === index) {
+      var value = todoForm.querySelector(".todo-input").value;
+      formUpdateIds.push({
+        index: +index,
+        name: value,
+      });
+      todoForm
+        .querySelector(".todo-input")
+        .addEventListener("change", function () {
+          value = this.value;
+          var indexUpdate = formUpdateIds.findIndex(function (element) {
+            return +element.index === +index;
+          });
+          if (indexUpdate !== -1) {
+            formUpdateIds[indexUpdate].name = value;
+          }
+        });
+
+      todoForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        todos[index].name = value; //Cập nhật lại mảng
+        todoForm.outerHTML = getTodoHtml(todos[index], index);
+        formUpdateIds = formUpdateIds.filter(function (element) {
+          return +element.index !== +index;
+        });
+      });
+    }
   });
+};
 
-  delet.addEventListener("click", function () {
-    task_list.removeChild(task_wrapper);
-  });
+todoForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-  label.addEventListener("click", function () {
-    edit.style.display = "block";
-    delet.style.display = "block";
-    label.style.display = "none";
-    task_input.style.width = "85%";
-    task_input.style.border = "none";
+  var nameEl = this.querySelector(".todo-input");
 
-    task_wrapper.style.background = "#8758ff";
-    // task_wrapper.style.gap = "10px";
-    task_wrapper.style.padding = "4px 16px";
-  });
+  var doName = nameEl.value;
 
-  task_wrapper.appendChild(task_input);
-  task_wrapper.appendChild(edit);
-  task_wrapper.appendChild(delet);
-  task_wrapper.appendChild(label);
-  task_list.appendChild(task_wrapper);
+  if (!doName) {
+    alert("Vui lòng nhập tên");
+    return;
+  }
 
-  app_input.value = "";
-}
+  var todo = {
+    name: doName,
+    completed: false,
+  };
 
-btn_add.addEventListener("click", addTask);
-// Enter
-app_input.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    addTask();
+  todos.push(todo);
+
+  renderTodos();
+
+  nameEl.value = "";
+});
+
+todoList.addEventListener("click", function (e) {
+  var type;
+  var todoEl;
+  if (e.target.localName === "path") {
+    type = e.target.parentElement.dataset.type;
+    todoEl = e.target.parentElement.parentElement.parentElement;
+  } else {
+    type = e.target.dataset.type;
+    todoEl = e.target.parentElement.parentElement;
+  }
+
+  var index = todoEl.dataset.index;
+
+  if (type === "update") {
+    showFormUpdate(todoEl);
+  }
+
+  if (type === "remove") {
+    handleRemove(index);
+  }
+
+  if (type === "completed") {
+    todoEl = e.target.parentElement;
+    index = todoEl.dataset.index;
+    handleCompleted(index);
   }
 });
